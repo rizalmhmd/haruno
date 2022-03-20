d8b1469eead1e0cf72016ed0e2e92f5dd391f072
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-20 22:30:23 +0700

    Update viewonce.js

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     if (!m.quoted) throw `balas pesan yang hanya bisa dilihat sekali`
     try {
-        await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: true })
+        await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: false })
     } catch (e) {
         throw `balas pesan yang hanya bisa dilihat sekali`
     }
